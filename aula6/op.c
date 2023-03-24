e014edc61a15a1cceb95932c1bd133c9c89c0e79
erickmohr <erick.mmohr@gmail.com>
2023-03-23 22:10:51 -0300

    Operadores em C

---
+++ aula6/op.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/*
+ * = atribuicao
+ * + - unarios
+ * + - binarios (adicao e subtracao)
+ * * / multiplicacao e divisao
+ * % resto da divisao
+ * ++ incremento
+ * -- decremento
+ */
+
+
+ int main(void) {
+   int a ;
+
+   a =(4 + 2)*3;
+   printf("%d\n", a--);
+
+   return 0;
+}
+
